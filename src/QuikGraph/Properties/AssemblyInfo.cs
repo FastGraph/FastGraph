f83c4883cbe525af5961020a6e45e3e225d85433
Alexandre Rabérin <kernelith@live.fr>
2020-05-10 23:42:49 +0200

    Make JetBrains annotations a shared file.

--- src/QuikGraph/Properties/AssemblyInfo.cs
+++ src/QuikGraph/Properties/AssemblyInfo.cs
@@ -1,5 +1,3 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("QuikGraph.Serialization")]
 [assembly: InternalsVisibleTo("QuikGraph.Tests")]
-[assembly: InternalsVisibleTo("QuikGraph.Serialization.Tests")]
