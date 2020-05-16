13a971eaf7c67cde4ee71d05f044828029dfcdbc
Alexandre Rabérin <kernelith@live.fr>
2020-05-16 19:31:32 +0200

    Add font conversion extensions and test project for Graphviz module.

---
+++ src/QuikGraph.Graphviz/Extensions/BasicStructuresExtensions.cs
@@ -0,0 +1,36 @@
+﻿#if SUPPORTS_BASIC_EXTENSIONS
+using System.Drawing;
+using JetBrains.Annotations;
+using QuikGraph.Graphviz.Dot;
+
+namespace QuikGraph.Graphviz
+{
+    /// <summary>
+    /// Extensions related to basic structures types.
+    /// </summary>
+    public static class BasicStructuresExtensions
+    {
+        /// <summary>
+        /// Converts a <see cref="GraphvizFont"/> into a <see cref="Font"/>.
+        /// </summary>
+        [Pure]
+        [CanBeNull]
+        [ContractAnnotation("font:null => null; font:notnull => notnull")]
+        public static Font ToFont([CanBeNull] this GraphvizFont font)
+        {
+            return font is null ? null : new Font(font.Name, font.SizeInPoints);
+        }
+
+        /// <summary>
+        /// Converts a <see cref="Font"/> into a <see cref="GraphvizFont"/>.
+        /// </summary>
+        [Pure]
+        [CanBeNull]
+        [ContractAnnotation("font:null => null; font:notnull => notnull")]
+        public static GraphvizFont ToGraphvizFont([CanBeNull] this Font font)
+        {
+            return font is null ? null : new GraphvizFont(font.Name, font.SizeInPoints);
+        }
+    }
+}
+#endif
---
+++ tests/QuikGraph.GraphViz.Tests/Extensions/BasicStructuresExtensionsTests.cs
@@ -0,0 +1,68 @@
+﻿#if SUPPORTS_FONT
+using System.Drawing;
+using NUnit.Framework;
+using QuikGraph.Graphviz.Dot;
+
+namespace QuikGraph.Graphviz.Tests
+{
+    /// <summary>
+    /// Tests for <see cref="BasicStructuresExtensions"/>.
+    /// </summary>
+    [TestFixture]
+    internal class BasicStructuresExtensionsTests
+    {
+        #region Test helpers
+
+        private static void AssertEqual(Font font, GraphvizFont qFont)
+        {
+            if (font is null)
+            {
+                Assert.IsNull(qFont);
+            }
+            else
+            {
+                Assert.AreEqual(qFont.Name, font.Name);
+                Assert.AreEqual(qFont.SizeInPoints, font.SizeInPoints);
+            }
+        }
+
+        #endregion
+
+        [Test]
+        public void ToFont()
+        {
+            var qFont = new GraphvizFont("Microsoft Sans Serif", 12.5f);
+            Font font = qFont.ToFont();
+            AssertEqual(font, qFont);
+
+            qFont = new GraphvizFont("Comic Sans MS", 25.0f);
+            font = qFont.ToFont();
+            AssertEqual(font, qFont);
+
+            // ReSharper disable ExpressionIsAlwaysNull
+            qFont = null;
+            font = qFont.ToFont();
+            AssertEqual(font, qFont);
+            // ReSharper restore ExpressionIsAlwaysNull
+        }
+
+        [Test]
+        public void ToGraphvizFont()
+        {
+            var font = new Font("Microsoft Sans Serif", 12.5f);
+            GraphvizFont qFont = font.ToGraphvizFont();
+            AssertEqual(font, qFont);
+
+            font = new Font("Comic Sans MS", 25.0f);
+            qFont = font.ToGraphvizFont();
+            AssertEqual(font, qFont);
+
+            // ReSharper disable ExpressionIsAlwaysNull
+            font = null;
+            qFont = font.ToGraphvizFont();
+            AssertEqual(font, qFont);
+            // ReSharper restore ExpressionIsAlwaysNull
+        }
+    }
+}
+#endif
