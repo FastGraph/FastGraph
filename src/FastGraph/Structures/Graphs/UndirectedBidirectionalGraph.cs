c29cbe17f4ebcbf4deab78338a26a82ca87fe18b
Bruce Markham <219281+brucificus@users.noreply.github.com>
2021-12-12 13:07:26 -0500

    🔥 Removes TypeCode Compatibility

--- src/FastGraph/Structures/Graphs/UndirectedBidirectionalGraph.cs
+++ src/FastGraph/Structures/Graphs/UndirectedBidirectionalGraph.cs
@@ -1,10 +1,7 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-#if !SUPPORTS_TYPE_FULL_FEATURES
-using System.Reflection;
-#endif
 #if SUPPORTS_SERIALIZATION && NETSTANDARD2_0_OR_GREATER
 using System.Runtime.Serialization;
 using System.Security.Permissions;
@@ -38,11 +35,7 @@ namespace FastGraph
         {
             OriginalGraph = originalGraph ?? throw new ArgumentNullException(nameof(originalGraph));
 
-#if SUPPORTS_TYPE_FULL_FEATURES
             _reorder = typeof(IUndirectedEdge<TVertex>).IsAssignableFrom(typeof(TEdge))
-#else
-            _reorder = typeof(IUndirectedEdge<TVertex>).GetTypeInfo().IsAssignableFrom(typeof(TEdge).GetTypeInfo())
-#endif
                 ? (ReorderVertices)((TVertex source, TVertex target, out TVertex orderedSource, out TVertex orderedTarget) =>
                 {
                     if (Comparer<TVertex>.Default.Compare(source, target) > 0)
